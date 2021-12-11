a119a58dc9e854abf7d609873d27568f71b696f1
David Minge <36607089+mingd0@users.noreply.github.com>
2021-12-10 23:26:24 -0600

    Remove yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import Header from "./components/Header";
 import Counter from "./components/Counter";
@@ -17,8 +17,8 @@ function App() {
   // Jan 19, 2021 @ 6am
   // const roleStart = useMemo(() => new Date(2021, 0, 19, 6), [])
 
-  const [quote, setQuote] = useState()
-  const [author, setAuthor] = useState()
+  // const [quote, setQuote] = useState()
+  // const [author, setAuthor] = useState()
 
   const [hitchSeconds, setHitchSeconds] = useState(
     Math.floor((hitchEnd - new Date()) / 1000)
@@ -27,41 +27,12 @@ function App() {
     Math.floor((roleEnd - new Date()) / 1000)
   )
 
-  const getInspiration = async () => {
-    const res = await fetch("https://zenquotes.io/api/random/")
-    // const res = await fetch("http://quotes.rest/qod/")
-    // const res = await fetch("https://api.quotable.io/random")
-    // const res = await fetch("https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731", {
-    //   "method": "GET",
-    //   "headers": {
-    //     "x-rapidapi-host": "healthruwords.p.rapidapi.com",
-    //     "x-rapidapi-key": "52c0a96bc9msh1e602d8f4585865p15a209jsn9b289a2b8984"
-    //   }
-    // })
-    // const options = {
-    //   method: 'GET',
-    //   url: 'https://healthruwords.p.rapidapi.com/v1/quotes/',
-    //   params: {t: 'Wisdom', maxR: '1'},
-    //   headers: {
-    //     'x-rapidapi-host': 'healthruwords.p.rapidapi.com',
-    //     'x-rapidapi-key': '52c0a96bc9msh1e602d8f4585865p15a209jsn9b289a2b8984'
-    //   }
-    // };
-    // const res = await axios.request(options)
-    const data = await res.json()
-    // console.log(data)
-    setQuote(data[0].q)
-    setAuthor(data[0].a)
-    // alert(`${data[0].q} - ${data[0].a}`)
-  }
-  // const testFetch = async () => {
-  //   const res = await fetch("https://localhost:5000/getQuote/")
+  // const getInspiration = async () => {
+  //   const res = await fetch("https://zenquotes.io/api/random/")
   //   const data = await res.json()
-  //   console.log(data)
-  // setQuote(data[0].q)
-  // setAuthor(data[0].a)
-  // alert(`${data[0].q} - ${data[0].a}`)
-// }
+  //   setQuote(data[0].q)
+  //   setAuthor(data[0].a)
+  // }
 
 useEffect(() => {
   const interval = setInterval(() => setHitchSeconds(
@@ -92,8 +63,8 @@ return (
     <Counter label='Approximate Seconds Remaining in Role'
       value={roleSeconds}
     />
-    <Button text='Click for Inspiration' color='black' onClick={getInspiration} />
-    {quote && <QuoteBox quote={quote} author={author} />}
+    {/* <Button text='Click for Inspiration' color='black' onClick={getInspiration} />
+    {quote && <QuoteBox quote={quote} author={author} />} */}
   </div>
 );
 }
